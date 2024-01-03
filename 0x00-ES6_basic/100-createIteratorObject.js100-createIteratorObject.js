31d5fc3a8e77c97570f39340d4375867fe40e65b
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-01-03 12:39:01 +0100

    100

--- 0x00-ES6_basic/100-createIteratorObject.js100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const objValues = [];
   for (const val of Object.values(report.allEmployees)) {
-    result.push(...val);
+    objValues.push(...val);
   }
   return objValues;
 }
