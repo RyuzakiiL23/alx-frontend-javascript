9dd93e1f4ce1b787f144a0193ecba5a21885b47d
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-01-03 08:44:35 +0100

    eslint

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,16 @@
-import getBudgetObject from './7-getBudgetObject.js'
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject (income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita)
+  const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
     getIncomeInDollars: function (income) {
-      return `$${income}`
+      return `$${income}`;
     },
     getIncomeInEuros: function (income) {
-      return `${income} euros`
-    }
-  }
+      return `${income} euros`;
+    };
+  };
 
-  return fullBudget
-}
+  return fullBudget;
+};
