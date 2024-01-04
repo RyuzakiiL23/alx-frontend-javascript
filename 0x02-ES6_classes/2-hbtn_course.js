8a5a8dd4d5cf7d7b9f3f9d645ef9440b294bdbe2
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-01-04 11:57:39 +0100

    classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,55 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(update) {
+    if (typeof update !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = update;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(num) {
+    if (typeof num !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = num;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(arr) {
+    if (!Array.isArray(arr)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = arr;
+    }
   }
 }
